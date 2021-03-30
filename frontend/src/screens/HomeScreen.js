ed6e009c01d6ad8242ea75af9876bf94835b55be
Tayyab Awan <tayyabaliawan1997@gmail.com>
2021-03-30 21:34:41 +0500

    redux configured, products data fetched with redux actions

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 import { Row, Col, Container } from 'react-bootstrap';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList);
+    const { loading, error, products } = productList;
 
-    const [products, setProducts] = useState([]);
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-            setProducts(data);
-        }
-        fetchProducts();
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
 
     return (
         <Container>
             <main className="my-5">
                 <h1>Latest Products</h1>
-                <Row>
-                    {
-                        products && products.map(product => (
-                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                <Product product={product} />
-                            </Col>
-                        ))
-                    }
-                </Row>
+                {
+                    loading ?
+                        <Loader />
+                        :
+                        error ?
+                            <Message variant='danger' children={error} />
+                            :
+                            <Row>
+                                {
+                                    products && products.map(product => (
+                                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                            <Product product={product} />
+                                        </Col>
+                                    ))
+                                }
+                            </Row>
+                }
             </main>
         </Container>
     )
