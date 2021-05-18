1ce908ba01ee01284f6bf804adbe367875bb3861
Tayyab Awan <tayyabaliawan1997@gmail.com>
2021-05-18 16:33:02 +0500

    change for testing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,7 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     if (userExists) {
         res.status(400);
-        throw new Error('User already exists')
+        throw new Error('User already exists!')
     }
     const user = await User.create({
         name,
